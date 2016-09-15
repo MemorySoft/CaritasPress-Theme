03afbebde7af1351042d0e8d8487fdb9b02453a1
Hector Asencio <cranca@gmail.com>
2016-09-15 10:41:03 +0200

    Agraga un nuevo sidebar
    Se ha añadido un nuevo sidebar para contener widgets en las plantillas
    templates/sidebar-left.php y templates/sidebar-right.php

--- includes/funciones/func_sidebars.php
+++ includes/funciones/func_sidebars.php
@@ -9,6 +9,16 @@
 *  	@package CaritasPress
 */
 if ( function_exists( 'register_sidebar' ) ) {
+	// SIDEBAR
+	register_sidebar( array(
+		'name' => 'Barra Lateral',
+		'id' => 'sidebar',
+		'description'   => 'Aquest contingut només es veurá en les plantilles "Sidebar a la izquierda" i "Sidebar a la derecha"',
+		'before_widget' => '<div id="%1$s" class="modulo %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '',
+		'after_title' => '',
+	) );
 	// PORTADA
 	register_sidebar( array(
 		'name' => 'Portada Dalt',

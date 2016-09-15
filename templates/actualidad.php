63ab173c50a17bf60da072d3ab46374a4de6b8fe
Hector Asencio <cranca@gmail.com>
2016-09-15 10:54:19 +0200

    Actualiza el código
    Tenia el código de la maqueta y el contenido de Actualidad se
    renderizaba via page.php, ahora está todo en su sitio

--- templates/actualidad.php
+++ templates/actualidad.php
@@ -1,231 +1,141 @@
-<?php /* Template Name: Actualidad */ ?>
-<?php include('includes/opciones/_variables.php'); ?>
 <?php get_header(); ?>
 
-<!-- INTRO -->
-    
-<div class="row sin-margen--abajo texto-centrado">
-  <div class="small-12 columns">
-    <h2 class="sin-margen--abajo">Actualitat i noticies de Càritas Menorca</h2>
+<!-- TITULO -->
+
+<div class="row sin-margen--abajo">
+  <div class="small-12 columns texto-centrado">
+      <?php if ( is_category() ) { ?>
+        <h2 class="pagina-titulo">Categoria: <?php single_cat_title(); ?></h2>
+      <?php } elseif ( is_tag() ) { ?>
+        <h2 class="pagina-titulo">Etiqueta: <?php single_tag_title(); ?></h2>
+      <?php } elseif ( is_day() ) { ?>
+        <h2 class="pagina-titulo"><?php the_time('j \d\e\ F \d\e\ Y'); ?></h2>
+      <?php } elseif ( is_month() ) { ?>
+        <h2 class="pagina-titulo"><?php the_time('F \d\e\ Y'); ?></h2>
+      <?php } elseif ( is_year() ) { ?>
+        <h2 class="pagina-titulo"><?php the_time('Y'); ?></h2>
+      <?php } elseif ( is_author() ) { ?>
+        <h2 class="pagina-titulo">Articles de <?php get_the_author();?></h2>
+      <?php } elseif ( isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+        <h2 class="pagina-titulo">Artxiu de <?php bloginfo( 'name' ); ?></h2>
+        </div>
+      </div>  
+      <?php } else { ?>
+        <h2 class="sin-margen--abajo">Actualitat i noticies de Càritas Menorca</h2>
+        <p class="texto-destacado">Últimes notícies publicades al bloc</p>
+      <?php } ?>
   </div>
-</div>
+  <hr>
+</div>  
 
 <!-- FILTROS -->
-
-<div class="row">
-  <form class="navegacion-noticias" id="formularioNavegacion" action="">
-    <div class="small-12 medium-3 columns">
-      <select name="formulario-navegacion--categorias" id="formularioNavegaciónCategorias">
-        
-        <option value="Categoria 1">CATEGORIES</option>
-        <option value="Categoria 2">Categoria 2</option>
-        <option value="Categoria 3">Categoria 3</option>
-        <option value="Categoria 4">Categoria 4</option>
-        <option value="Categoria 5">Categoria 5</option>
-      </select>
-    </div>
-    <div class="small-12 medium-3 columns">
-      <select name="formulario-navegacion--categorias" id="formularioNavegaciónCategorias">
-        
-        <option value="Etiqueta 1">ETIQUETES</option>
-        <option value="Etiqueta 2">Etiqueta 2</option>
-        <option value="Etiqueta 3">Etiqueta 3</option>
-        <option value="Etiqueta 4">Etiqueta 4</option>
-        <option value="Etiqueta 5">Etiqueta 5</option>
-      </select>
-    </div>    
-    <div class="small-12 medium-6 columns">
-      <div class="input-group">
-        <input class="input-group-field" type="text" placeholder="CERCAR">
-        <div class="input-group-button">
-          <button class="button"><i class="fa fa-search"></i></button>
+<div class="espacio"></div>
+<div class="row sin-margen--abajo">
+  <div class="small-12 medium-6 columns">
+    <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <!--<label>Cercar</label> -->
+        <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+        <div class="input-group">
+          <input class="input-group-field" type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+          <div class="input-group-button">
+            <input class="button" type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+          </div>
         </div>
-      </div>
+    </form>
+    <a href="javascript:void(0)" class="button small secondary hide-for-medium -filtro">Filtra el contingut</a>
+  </div>
+  <div class="formulario-navegacion">
+  <form class="navegacion-noticias show-for-medium" id="formularioNavegacion" action="">
+    <div class="small-12 medium-3 columns">
+      <!--<label>Categories</label> -->
+      <?php 
+      $args = array(
+        'show_option_none' => 'Sel·lecciona una categoria',
+      ); ?>
+      <?php wp_dropdown_categories('show_option_none=Sel·lecciona una categoria'); ?>
+      <script type="text/javascript">
+        <!--
+        var dropdown = document.getElementById("cat");
+        function onCatChange() {
+          if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
+            location.href = "<?php echo esc_url( home_url( '/' ) ); ?>?cat="+dropdown.options[dropdown.selectedIndex].value;
+          }
+        }
+        dropdown.onchange = onCatChange;
+        -->
+      </script>
     </div>
-  </form>
+    <div class="small-12 medium-3 columns end">
+      <!--<label>Etiquetes</label> -->
+      <?php 
+      $args = array(
+        'taxonomy' => 'post_tag',
+        'show_option_none' => 'Sel·lecciona una etiqueta',
+      ); ?>
+      <?php wp_dropdown_categories( $args ); ?>
+      <script type="text/javascript">
+        <!--
+        var dropdown = document.getElementById("cat");
+        function onCatChange() {
+          if ( dropdown.options[dropdown.selectedIndex].value > 0 ) {
+            location.href = "<?php echo esc_url( home_url( '/' ) ); ?>?cat="+dropdown.options[dropdown.selectedIndex].value;
+          }
+        }
+        dropdown.onchange = onCatChange;
+        -->
+      </script>
+    </div> 
+  </form> 
+  </div>  
+</div>
+
+<div class="row sin-margen--abajo espacio">
+  <div class="small-12 columns">
+    <?php if ( is_search() ) { ?>
+      <h4 class="texto-centrado">
+        <?php printf( __( 'Resultats de la cerca: %s', 'shape' ), get_search_query()  ); ?>  
+      </h4>
+    <?php } ?>
+  </div>
 </div>
 
 <!-- NOTICIAS -->
 
-<div class="row small-up-1 medium-up-2 large-up-3">     
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
+<div class="row small-up-1 medium-up-2 large-up-3">   
+  <?php if(have_posts()) : ?>
+    <?php while(have_posts()) : the_post(); ?>  
+      <div class="column">
+        <div class="articulo stack-for-small texto centrado">
+          <div class="articulo-seccion articulo-seccion--vertical">
+            <div class="articulo-imagen">
+              <a href="<?php echo the_permalink(); ?>" title="<?php esc_attr__('Llegir','caritaspress'); ?> <?php the_title(); ?>">
+                <?php the_post_thumbnail(); ?>
+              </a>
+            </div>
+          </div>
+          <div class="articulo-seccion articulo-seccion--vertical">
+            <h4 class="articulo-titulo"><?php the_title(); ?></h4>
+            <p class="articulo-extracto"><?php echo the_excerpt(); ?></p>
+            <a href="<?php echo the_permalink(); ?>" class="button" title="<?php esc_attr__('Llegir','caritaspress'); ?> <?php the_title(); ?>"><?php _e('Llegir mès','caritaspress' ); ?></a>
+          </div>
         </div>
       </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolor pariatur, numquam officia sint quos nobis, iste. Debitis rerum repellendus beatae eaque molestiae laborum vitae ad rem illo quos suscipit, impedit dolor, consectetur fugit, similique corrupti quaerat deleniti eius quam?</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum reprehenderit eligendi laudantium tempore, asperiores maxime, porro non impedit. Earum nobis vitae consequatur quo assumenda distinctio non, quidem dicta vel aut eius praesentium veritatis, animi eum quasi. Harum placeat nobis, distinctio.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia doloremque consequuntur culpa temporibus iure harum placeat ullam. Nostrum quia veniam quae voluptas aspernatur harum doloribus quisquam quod perspiciatis architecto, dolorem placeat deserunt, laboriosam aperiam reprehenderit. Eaque dicta aspernatur, doloremque optio!</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, impedit, aut. Consequuntur ipsum eum praesentium adipisci quibusdam, velit amet voluptas perferendis temporibus id rerum, magnam, itaque. Error blanditiis labore vitae expedita fugiat, atque mollitia tenetur asperiores praesentium, nobis nemo vel.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ratione, iure tempora ullam numquam fugiat id, facere sunt veritatis ducimus tempore quo laborum vel dignissimos harum dicta similique rerum quisquam recusandae natus reiciendis libero nostrum. Itaque sunt, architecto laudantium delectus.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis est reiciendis suscipit eligendi, earum ratione non commodi debitis et molestiae vitae. Facere, doloribus suscipit a, ullam veritatis commodi magnam harum dolores necessitatibus quos! Ex ab, iste est tenetur optio nihil.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia placeat, autem voluptatem rem beatae nemo soluta ullam deleniti. Illum assumenda quibusdam nulla qui dicta labore enim distinctio atque iusto est, nesciunt, corrupti reiciendis dolorum ipsum, facilis! Nobis eum, maxime similique.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut exercitationem, a distinctio consectetur nulla sequi qui saepe. Qui aliquid, maxime beatae rem quas dolor praesentium, odio quibusdam dignissimos animi voluptates! Quis molestiae atque asperiores nam nemo, alias maiores harum voluptate?</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus architecto ab veritatis quam, quisquam quas quia at temporibus vero alias labore minima adipisci iure! Explicabo voluptatem fugiat dolore pariatur mollitia. Laborum deserunt aspernatur facilis inventore vitae eligendi unde dicta, maxime!</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus deleniti aliquid autem beatae, sunt eligendi deserunt necessitatibus eius, quisquam facilis quia laboriosam debitis distinctio modi corporis dolorem ducimus consequuntur illo aut? Necessitatibus quos, harum dolor facilis corrupti voluptatem consequatur natus.</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sequi veniam enim officia totam, hic. Ducimus numquam culpa impedit fugit. Illo dolorum consectetur et nam cum iure nemo facere. Sit reiciendis consectetur explicabo perspiciatis, similique porro, expedita rem corporis quos!</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
-  <div class="column">
-    <div class="articulo stack-for-small texto centrado">
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <div class="thumbnail">
-          <img class="articulo-imagen" src="http://placehold.it/350x250" alt="">
-        </div>
-      </div>
-      <div class="articulo-seccion articulo-seccion--vertical">
-        <h4 class="articulo-titulo">Lorem ipsum dolor</h4>
-        <p class="articulo-extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sequi veniam enim officia totam, hic. Ducimus numquam culpa impedit fugit. Illo dolorum consectetur et nam cum iure nemo facere. Sit reiciendis consectetur explicabo perspiciatis, similique porro, expedita rem corporis quos!</p>
-        <a href="noticia.html" class="button">Llegir tot</a>
-      </div>
-    </div>
-  </div>
+    <?php endwhile; ?>
+  <?php endif; ?>
 </div>
 
-<div class="row">
-  <div class="small-12 columns">
-    <hr>
-    <ul class="pagination text-center" role="navigation" aria-label="Pagination">
-      <li class="pagination-prev disabled"><a href="#" aria-label="Anteriors">Noticies recents</a></li>
-      <li class="pagination-next"><a href="#" aria-label="Propers">Noticies antigues</a></li>
-    </ul>
-  </div>
-</div>
+<!-- PAGINADOR -->
 
+<?php if (previous_posts_link() != '' || next_posts_link() != '' ) { ?>
+  <div class="row">
+    <div class="small-12 columns">
+      <hr>
+      <ul class="pagination text-center" role="navigation" aria-label="Pagination">
+        <li class="pagination-prev"><?php next_posts_link( 'Noticies recents' ); ?></li>
+        <li class="pagination-next"><?php previous_posts_link( 'Noticies antigues' ); ?></li>
+      </ul>
+    </div>
+  </div>
+<?php } ?>
+<hr>
 <?php get_footer(); ?>

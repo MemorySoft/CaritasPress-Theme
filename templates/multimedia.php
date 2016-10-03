f14533804ad4ac8b3362863fe9035631ef76958d
Hector Asencio <cranca@gmail.com>
2016-10-03 18:56:55 +0200

    Renderiza los archivos multimedia con fecha y descripción

--- templates/multimedia.php
+++ templates/multimedia.php
@@ -3,41 +3,59 @@
 <?php get_header(); ?>
 
 <!-- CONTENIDO -->
-    
-    <div class="row sin-margen--abajo">
-      <div class="small-12 columns">
-        <h3 class="titulo">Multimedia</h3>
-      </div>
-    </div>
 
-    <div class="franja fondo-gris--claro">
-      <div class="row sin-margen--abajo">
-        <h2 class="articulo-titulo texto-mayuscula">Cáritas al Youtube</h2>
-        <p class="texto-destacado">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio rerum nesciunt ea maxime, dolore, porro pariatur tempore ad excepturi praesentium quos officiis eaque repudiandae iusto amet earum iure dolorem doloribus consequatur error accusamus. Dolorem, fuga.</p>
-        <a href="https://www.youtube.com/channel/UCzcLy0ubLrH_QOmelsHbqcQ" target="_blank" class="large button">Veure el canal de Càritas</a>
-      </div>
-    </div>
+<div class="row sin-margen--abajo">
+  <div class="small-12 columns">
+    <h3 class="titulo">Multimedia</h3>
+  </div>
+</div>
 
-    <div class="row">  
-      <?php 
-        $audio_args = array(
-        'post_type' => 'audio',
-        'posts_per_page'=> 100,
-      );
-      $wp_query = new WP_Query($audio_args);
-      if( $wp_query->have_posts() ) { ?>       
-        <ul class="coleccion">
-          <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-            <?php $enlace = get_post_meta( get_the_id(), 'audio_enlace', true ); ?>
-            <li class="coleccion-elemento">
-              <span class="titulo"><i class="fa fa-volume-up"></i> <?php the_title(); ?></span>
-              <a class="coleccion-elemento--secundario" href="<?php echo $enlace ?>" target="_blank">
-                <i class="fa fa-download"></i>
+<!-- YOUTUBE -->
+
+<div class="franja fondo-gris--claro">
+  <div class="row sin-margen--abajo">
+    <h2 class="articulo-titulo texto-mayuscula">Cáritas al YouTube</h2>
+    <p class="texto-destacado">Pots veure tots els videos de Cáritas Menorca al nostre canal al YouTube</p>
+    <a href="https://www.youtube.com/channel/UCzcLy0ubLrH_QOmelsHbqcQ" target="_blank" class="large button">Veure el canal de Càritas</a>
+  </div>
+</div>
+
+<!-- AUDIO -->
+
+<div class="row">
+  <div class="small-12 columns">
+    <h3 class="titulo">Audios</h3>
+  </div>
+
+  <div class="small-12 columns">
+    <?php
+      $audio_args = array(
+      'post_type' => 'audio',
+    );
+    $wp_query = new WP_Query($audio_args);
+    if( $wp_query->have_posts() ) { ?>
+      <ul class="coleccion">
+        <?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+          <?php $audio_enlace = get_post_meta( get_the_id(), 'audio_enlace', true ); ?>
+          <?php $audio_fecha = get_post_meta( get_the_id(), 'audio_fecha', true ); ?>
+          <li class="coleccion-elemento">
+            <div class="coleccion-titulo">
+              <a href="<?php echo $audio_enlace ?>" target="_blank">
+                <?php the_title(); ?>
               </a>
-            </li>
-          <?php endwhile; ?>
-        </ul>
-      <?php } ?>
-    </div>
+            </div>
+            <small class="coleccion-fecha"><?php echo $audio_fecha ?></small>
+            <p class="coleccion-descripcion"><?php echo the_content(); ?></p>
+            <a class="coleccion-elemento--secundario boton-flotante--invertido" href="<?php echo $audio_enlace ?>" target="_blank">
+              <i class="fa fa-volume-up"></i>
+            </a>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+    <?php } ?>
+  </div>
+</div>
+
+<hr>
 
 <?php get_footer(); ?>

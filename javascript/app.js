a391e2b48c4f19e031d30aa75c8eb745254d477c
Hector Asencio <cranca@gmail.com>
2016-10-03 18:39:28 +0200

    TouchTouch

--- javascript/app.js
+++ javascript/app.js
@@ -5,17 +5,19 @@ $(document).ready(function() {
 
   // CARRUSELES
   $(".-carrusel-un-item").owlCarousel({
-    autoPlay: true,
+    // autoPlay: true,
     slideSpeed : 300,
     paginationSpeed : 400,
     singleItem:true,
-    pagination : false
+    pagination : false,
+    navigation: true,
+    navigationText: ["«","»"],
   });
 
   $(".-carrusel-tres-items").owlCarousel({
     autoPlay: true,
     navigation: true,
-    navigationText: ["←","→"],
+    navigationText: ["«","»"],
     pagination: false,
     items : 3,
     itemsDesktop : [1200,3],
@@ -25,12 +27,15 @@ $(document).ready(function() {
   $(".-carrusel-cuatro-items").owlCarousel({
     autoPlay: true,
     navigation: true,
-    navigationText: ["←","→"],
+    navigationText: ["«","»"],
     pagination: false,
     items : 4,
     itemsDesktop : [1200,3],
     itemsDesktopSmall : [400,1]
   });
+
+  // GALERIAS
+  $('.gallery-item a').touchTouch();
 
   // MENU
   $(".menu-disparador").click(function() {

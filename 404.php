58fd62e674878cae216ea2b58e0403e21de54b84
Hector Asencio <cranca@gmail.com>
2016-10-03 18:33:17 +0200

    Agrega contenido a la página de error

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="contenedor">
+
+  <!-- CONTENIDO -->
+
+  <div class="proyectos">
+    <div class="row texto-centrado">
+      <div class="small-12 columns">
+        <div class="large callout fondo-rojo texto-centrado">
+          <h1>Página no trobada</h1>
+          <p class="texto-destacado">Per alguna raó no s'ha pogut trobar la página o el contingut que demanaves.</p>
+          <div class="texto-destacado--peque sin-margen--abajo">Si creus que alló que cercaves hauria de trobar-se o sospites que es un error, posa't en contacte amb Càrites Diocesana de Menorca i explican's-ho. Gràcies!</div>
+        </div>
+        <p><a class="button" href="<?php echo site_url(); ?>/contacte">CONTACTAN'S</a></p>
+      </div>
+    </div>
+  </div>
+
+</div> <!-- /.contenedor -->
+
+<hr>
+
+<?php get_footer(); ?>

34e6f8fc0bb8bcdf1dafb499726c69a7f66bb1a3
Hector Asencio <cranca@gmail.com>
2016-10-13 17:09:40 +0200

    Refactoriza el layout

--- templates/multimedia.php
+++ templates/multimedia.php
@@ -6,25 +6,7 @@
 
 <div class="row sin-margen--abajo">
   <div class="small-12 columns">
-    <h3 class="titulo">Multimedia</h3>
-  </div>
-</div>
-
-<!-- YOUTUBE -->
-
-<div class="franja fondo-gris--claro">
-  <div class="row sin-margen--abajo">
-    <h2 class="articulo-titulo texto-mayuscula">Cáritas al YouTube</h2>
-    <p class="texto-destacado">Pots veure tots els videos de Cáritas Menorca al nostre canal al YouTube</p>
-    <a href="https://www.youtube.com/channel/UCzcLy0ubLrH_QOmelsHbqcQ" target="_blank" class="large button">Veure el canal de Càritas</a>
-  </div>
-</div>
-
-<!-- AUDIO -->
-
-<div class="row">
-  <div class="small-12 columns">
-    <h3 class="titulo">Audios</h3>
+    <h2 class="titulo">Multimedia</h2>
   </div>
 
   <div class="small-12 columns">
@@ -44,8 +26,8 @@
                 <?php the_title(); ?>
               </a>
             </div>
-            <small class="coleccion-fecha"><?php echo $audio_fecha ?></small>
-            <p class="coleccion-descripcion"><?php echo the_content(); ?></p>
+            <span class="coleccion-fecha"><strong><?php echo $audio_fecha ?></strong></span>
+            <span class="coleccion-descripcion"><?php echo the_content(); ?></span>
             <a class="coleccion-elemento--secundario boton-flotante--invertido" href="<?php echo $audio_enlace ?>" target="_blank">
               <i class="fa fa-volume-up"></i>
             </a>
@@ -56,6 +38,19 @@
   </div>
 </div>
 
-<hr>
+<!-- YOUTUBE -->
+
+<div class="row sin-margen--abajo">
+  <div class="small-12 medium-8 columns show-for-medium">
+    <h3 class="titulo texto-mayuscula sin-margen--abajo">
+      Cáritas al YouTube
+      <br><small>Visita el nostre canal de vídeos</small>
+    </h3>
+  </div>
+  <div class="small-12 medium-4 columns">
+    <a href="https://www.youtube.com/channel/UCzcLy0ubLrH_QOmelsHbqcQ" target="_blank" class="expanded large button boton-iconizado"><i class="fa fa-youtube "></i> Càritas Menorca</a>
+  </div>
+  <hr>
+</div>
 
 <?php get_footer(); ?>

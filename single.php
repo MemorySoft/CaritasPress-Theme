9f32072fb76d08456ed2d75f401f30a8c491467e
Hector Asencio <cranca@gmail.com>
2016-10-13 17:10:41 +0200

    Mejora la vista para moviles

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
   <!-- CONTENIDO -->
 
-  <div class="row">
+  <div class="row sin-margen--abajo">
   	<div class="small-12 columns">
   		<?php if(have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
@@ -14,16 +14,16 @@
           <div class="row post">
             <?php edit_post_link('Editar', ''); ?>
             <div class="small-12 columns">
-              <h2 class="titulo post-titulo"><?php the_title(); ?></h2>
+              <h2 class="post-titulo"><?php the_title(); ?></h2>
             </div>
-            
+
             <hr>
-            <div class="espacio"></div>            
-           
+            <div class="espacio"></div>
+
             <div class="small-12 medium-4 columns post-lateral">
-              <?php the_post_thumbnail(); ?>  
+              <?php the_post_thumbnail(); ?>
               <div class="post-metadatos">
-                <div class="post-metadatos--fecha">Publicat el <span><?php the_time('j \d\e\ F \d\e\ Y'); ?></span></div> 
+                <div class="post-metadatos--fecha">Publicat el <span><?php the_time('j \d\e\ F \d\e\ Y'); ?></span></div>
                 <div class="post-metadatos--categoria">
                   <span>Artxivat com:</span>
                   <?php the_category(); ?>
@@ -37,16 +37,16 @@
           </div>
 
         <?php endwhile; ?>
-    
+
         <!-- NAVEGACION POSTS -->
 
         <div class="row navegacion-articulos sin-margen--abajo">
           <hr>
-          <div class="small-12 medium-6 texto-centrado medium columns navegacion-anterior-articulo"> 
-            <?php previous_post_link( '<p>ARTÍCLE PREVI</p> %link', '%title' ); ?>
+          <div class="small-12 medium-6 texto-centrado medium columns navegacion-anterior-articulo">
+            <?php previous_post_link( '<p class="sin-margen--abajo">ARTÍCLE PREVI</p> %link', '%title' ); ?>
           </div>
           <div class="small-12 medium-6 texto-centrado medium columns navegacion-proximo-articulo">
-            <?php next_post_link( '<p>PRÓXIM ARTÍCLE</p> %link', '%title' ); ?>
+            <?php next_post_link( '<p class="sin-margen--abajo">PRÓXIM ARTÍCLE</p> %link', '%title' ); ?>
           </div>
         </div>
       <?php endif; ?>
